9229ea62171196aec9249d358c52933529dba757
juan <juan1280h@gmail.com>
2017-10-25 20:15:09 -0700

    Tarea 2: Ocultar o Mostrar el div donde se encuentra la lista y el input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   nombre = 'juan hernandez';
+  colores: string[];
+  showcolors: boolean;
+
+  constructor(){
+
+    this.colores =['negro', 'rojo', 'blanco'];
+    this.showcolors = false;
+  }
+  togglecolors(){
+    this.showcolors = !this.showcolors;
+  }
 }
